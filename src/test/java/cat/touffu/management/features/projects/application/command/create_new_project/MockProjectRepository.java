ebba8068f76b9be2006a9f7614b4498d1cf0938f
Nigth <omnes.theo@gmail.com>
2022-03-16 22:42:12 +0100

    add test for createNewProjectHandler

---
+++ src/test/java/cat/touffu/management/features/projects/application/command/create_new_project/MockProjectRepository.java
@@ -0,0 +1,35 @@
+package cat.touffu.management.features.projects.application.command.create_new_project;
+
+import cat.touffu.management.features.projects.domain.Project;
+import cat.touffu.management.features.projects.domain.ProjectId;
+import cat.touffu.management.features.projects.domain.ProjectRepository;
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MockProjectRepository implements ProjectRepository {
+
+    public Map<String, Project> store = new HashMap<>();
+
+    @Override
+    public void save(Project project) {
+        this.store.put(project.title(), project);
+    }
+
+    @Override
+    public Project findById(ProjectId projectId) {
+        throw new NotImplementedException("find project by id");
+    }
+
+    @Override
+    public List<Project> findAll() {
+        throw new NotImplementedException("find all projects");
+    }
+
+    @Override
+    public void remove(Project item) {
+        throw new NotImplementedException("remove project");
+    }
+}
