3510080ff8704a46f38d0a934e9afd5578b91bb2
Nigth <omnes.theo@gmail.com>
2022-03-19 11:29:20 +0100

    wip for Dependecy Injection with Guice

--- src/main/java/cat/touffu/management/Cli.java
+++ src/main/java/cat/touffu/management/Cli.java
@@ -1,6 +1,10 @@
 package cat.touffu.management;
 
+import cat.touffu.management.configuration.CDIFactory;
 import cat.touffu.management.features.projects.exposition.cli.NewProject;
+import cat.touffu.management.kernel.command.CommandBus;
+import cat.touffu.management.kernel.command.SimpleCommandBus;
+import com.google.inject.Inject;
 import picocli.CommandLine;
 
 @CommandLine.Command(
@@ -9,12 +13,16 @@ import picocli.CommandLine;
         }
 )
 public class Cli implements Runnable{
+    @Inject CommandBus commandBus;
+
     public static void main(String[] args) {
-        CommandLine.run(new Cli(), args);
+        new CommandLine(Cli.class, new CDIFactory()).execute(args);
     }
 
     @Override
     public void run() {
         System.out.println("Touffu management CLI.");
+        System.out.println(this.commandBus);
+        assert commandBus instanceof SimpleCommandBus;
     }
 }
