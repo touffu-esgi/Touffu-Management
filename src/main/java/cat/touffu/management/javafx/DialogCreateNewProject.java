7793c666bc7dbc26074d9b57d63b0b410d8c6123
Nigth <omnes.theo@gmail.com>
2022-06-13 23:17:17 +0200

    close stage on save and on cancel

--- src/main/java/cat/touffu/management/javafx/DialogCreateNewProject.java
+++ src/main/java/cat/touffu/management/javafx/DialogCreateNewProject.java
@@ -8,13 +8,18 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class DialogCreateNewProject extends Application {
+    public Stage stage;
+
     @Override
     public void start(Stage stage) throws Exception {
+        this.stage = stage;
         stage.setTitle("Add a project");
         StackPane stack = new StackPane();
         FXMLLoader root = new FXMLLoader(getClass().getResource("addProjectDialog.fxml"));
 
         stack.getChildren().add(root.load());
+        DialogCreateNewProjectController controller = root.getController();
+        controller.app = this;
 
         Scene scene = new Scene(stack,511,365, true);
         stage.setResizable(false);
--- src/main/java/cat/touffu/management/javafx/DialogCreateNewProjectController.java
+++ src/main/java/cat/touffu/management/javafx/DialogCreateNewProjectController.java
@@ -1,14 +1,32 @@
 package cat.touffu.management.javafx;
 
+import cat.touffu.management.components.projects.ProjectModule;
+import cat.touffu.management.components.projects.application.command.CreateNewProject.CreateNewProject;
+import cat.touffu.management.kernel.command.CommandBus;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
 public class DialogCreateNewProjectController{
 
+    public DialogCreateNewProject app;
+
 
     public TextField list_name;
+    private final CommandBus commandBus = ProjectModule.commandBus();
 
-    public void saveNewList(ActionEvent actionEvent) {
+    public void saveNewList(ActionEvent actionEvent) throws Exception {
         String value = list_name.getText();
+        commandBus.send(new CreateNewProject(value));
+        this.close();
     }
+
+    public void onCancel(ActionEvent actionEvent) throws Exception {
+        this.close();
+    }
+
+    private void close() throws Exception {
+        this.app.stage.close();
+        this.app.stop();
+    }
+
 }
