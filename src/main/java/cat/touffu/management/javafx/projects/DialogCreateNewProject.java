37f08bd15006822b4da024dc2c8228aedf02f244
Nigth <omnes.theo@gmail.com>
2022-06-18 15:07:30 +0200

    set contructor of dialog

--- src/main/java/cat/touffu/management/javafx/projects/DialogCreateNewProject.java
+++ src/main/java/cat/touffu/management/javafx/projects/DialogCreateNewProject.java
@@ -6,19 +6,28 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class DialogCreateNewProject extends Application {
+    private final StackPane stack;
     public Stage stage;
+    final DialogCreateNewProjectController controller;
+
+    public DialogCreateNewProject() {
+        this.stack = new StackPane();
+        FXMLLoader root = new FXMLLoader(getClass().getResource("addProjectDialog.fxml"));
+
+        try {stack.getChildren().add(root.load());}
+        catch (IOException e) {e.printStackTrace();}
+
+        this.controller = root.getController();
+        controller.app = this;
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
         stage.setTitle("Add a project");
-        StackPane stack = new StackPane();
-        FXMLLoader root = new FXMLLoader(getClass().getResource("addProjectDialog.fxml"));
-
-        stack.getChildren().add(root.load());
-        DialogCreateNewProjectController controller = root.getController();
-        controller.app = this;
 
         Scene scene = new Scene(stack,511,365, true);
         stage.setResizable(false);
