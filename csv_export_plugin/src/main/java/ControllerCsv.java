ab2ac0e5763363e24991bf80203421a84ee0ba0a
Nigth <omnes.theo@gmail.com>
2022-07-15 21:44:25 +0200

    add export plugin module

---
+++ csv_export_plugin/src/main/java/ControllerCsv.java
@@ -0,0 +1,5 @@
+import cat.touffu.management.javafx.Controller;
+
+public class ControllerCsv extends Controller {
+
+}
---
+++ csv_export_plugin/src/main/java/Plugin.java
@@ -0,0 +1,39 @@
+import cat.touffu.management.javafx.Controller;
+import cat.touffu.management.plugin.JavaFxPlugin;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
+public class Plugin implements JavaFxPlugin {
+
+    private final Controller controller;
+    private Node view;
+
+    public Plugin() {
+        this.controller = new ControllerCsv();
+        var loader = new FXMLLoader(getClass().getResource("view.fxml"));
+        loader.setController(this.controller);
+
+        try {
+            this.view = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "CsvExportPlugin";
+    }
+
+    @Override
+    public Controller getController() {
+        return this.controller;
+    }
+
+    @Override
+    public Node getView() {
+        return this.view;
+    }
+}
